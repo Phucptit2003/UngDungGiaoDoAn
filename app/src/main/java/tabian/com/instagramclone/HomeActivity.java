af953bd135b490e1aa2b32f97466a134f73cc3cc
Le Phuc <phuc.le@savvycom.vn>
2025-03-14 14:32:25 +0700

    Profile Toolbar and Menu

--- app/src/main/java/tabian/com/instagramclone/HomeActivity.java
+++ app/src/main/java/tabian/com/instagramclone/HomeActivity.java
@@ -1,12 +1,8 @@
 package tabian.com.instagramclone;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
@@ -15,9 +11,6 @@ import tabian.com.instagramclone.Utils.BottomNavigationViewHelper;
 public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = "HomeActivity";
-    private static final int ACTIVITY_NUM = 0;
-
-    private Context mContext = HomeActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +28,5 @@ public class HomeActivity extends AppCompatActivity {
         Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
-        Menu menu = bottomNavigationViewEx.getMenu();
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-        menuItem.setChecked(true);
     }
 }
